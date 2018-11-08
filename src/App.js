d245c4365aedcb89ff711a40eb7247129b874d0c
David Parkinson <david.parkinson@human.software>
2018-11-08 15:13:19 +0000

    rudimentary component structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Graph from './components/Graph'
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    latitude: 1,
+    longitude: 1
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>
+        <Graph latitude = {this.state.latitude} longitude = {this.state.longitude}/>
+        <input></input>
+        </div>
     );
   }
 }
